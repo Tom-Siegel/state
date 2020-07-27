09f443c312f34798e3a554810284b42926b14689
TosiHyper <tomsiegel@web.de>
2020-07-27 19:44:56 +0200

    use of version 1.3.0 of x-eventable

--- dist/state.js
+++ dist/state.js
@@ -59,106 +59,97 @@
     {
       1: [
         function (require, module, exports) {
-          (function (global, factory) {
-            typeof exports === "object" && typeof module !== "undefined"
-              ? (module.exports = factory())
-              : typeof define === "function" && define.amd
-              ? define(factory)
-              : Object.assign(global, factory());
-          })(this, function () {
-            "use strict";
-            function eventable() {
-              var mvoEvents = [];
-              var base = this;
+          function eventable() {
+            var mvoEvents = [];
+            var base = this;
 
-              this.on = function (type, callback) {
-                if (typeof callback === "function") {
-                  mvoEvents.push({
-                    type: type,
-                    callback: callback,
-                  });
-                } else {
-                  console.error("need a callback to listen to a event");
-                }
-              };
+            this.on = function (type, callback) {
+              if (typeof callback === "function") {
+                mvoEvents.push({
+                  type: type,
+                  callback: callback,
+                });
+              } else {
+                console.error("need a callback to listen to a event");
+              }
+            };
 
-              this.off = function (type, callback) {
-                if (
-                  typeof type !== "undefined" &&
-                  typeof callback !== "undefined"
-                ) {
-                  var len = mvoEvents.length - 1;
+            this.off = function (type, callback) {
+              if (
+                typeof type !== "undefined" &&
+                typeof callback !== "undefined"
+              ) {
+                var len = mvoEvents.length - 1;
 
-                  for (var i = lem; i >= 0; i++) {
-                    var listener = mvoEvents[i];
-
-                    if (
-                      type === listener.type &&
-                      callback === listener.callback
-                    ) {
-                      mvoEvents.splice(i, 1);
-                    }
-                  }
-                }
-              };
-
-              this.raise = function (options) {
-                var e = eventable.create(options);
-                var len = mvoEvents.length;
-
-                e.target = base;
-
-                for (var i = 0; i < len; i++) {
+                for (var i = lem; i >= 0; i++) {
                   var listener = mvoEvents[i];
 
-                  if (e.type === listener.type) {
-                    e.return = listener.callback(e);
+                  if (
+                    type === listener.type &&
+                    callback === listener.callback
+                  ) {
+                    mvoEvents.splice(i, 1);
                   }
+                }
+              }
+            };
 
-                  if (e.stop === true) break;
+            this.raise = function (options) {
+              var e = eventable.create(options);
+              var len = mvoEvents.length;
+
+              e.target = base;
+
+              for (var i = 0; i < len; i++) {
+                var listener = mvoEvents[i];
+
+                if (e.type === listener.type) {
+                  e.return = listener.callback(e);
                 }
 
-                return e;
-              };
-            }
-
-            eventable.create = function (options) {
-              var t = typeof options;
-
-              if (t === "undefined" || options == null) return new _event();
-              if (t === "string") return new _event(options);
-              if (t === "object") return Object.assign(new _event(), options);
-
-              return new _event();
-            };
-
-            eventable.raise = function (target, options) {
-              if (eventable.canRaise(target) === true) {
-                return target.raise(options);
+                if (e.stop === true) break;
               }
 
-              return null;
+              return e;
             };
+          }
 
-            eventable.canRaise = function (target) {
-              return (
-                typeof target === "object" && typeof target.raise === "function"
-              );
-            };
+          eventable.create = function (options) {
+            var t = typeof options;
 
-            function _event(t) {
-              this.target = null;
-              this.details = {};
-              this.stop = false;
-              this.type = typeof t === "string" ? t : "";
-              this.return = null;
+            if (t === "undefined" || options == null) return new _event();
+            if (t === "string") return new _event(options);
+            if (t === "object") return Object.assign(new _event(), options);
+
+            return new _event();
+          };
+
+          eventable.raise = function (target, options) {
+            if (eventable.canRaise(target) === true) {
+              return target.raise(options);
             }
 
-            return {
-              eventable: eventable,
-              _event: _event,
-            };
-          });
+            return null;
+          };
+
+          eventable.canRaise = function (target) {
+            return (
+              typeof target === "object" && typeof target.raise === "function"
+            );
+          };
+
+          function _event(t) {
+            this.target = null;
+            this.details = {};
+            this.stop = false;
+            this.type = typeof t === "string" ? t : "";
+            this.return = null;
+          }
+
+          module.exports = {
+            eventable: eventable,
+            _event: _event,
+          };
         },
         {},
       ],
